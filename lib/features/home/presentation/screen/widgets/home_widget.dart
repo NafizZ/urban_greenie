cf72a40c9bee225936f91853f3e47b10eb13ac9f
Nafiz <shahnafizmanjoor@gmail.com>
2023-10-25 02:39:27 +0600

    home screen testing card made by hard code

--- lib/features/home/presentation/screen/widgets/home_widget.dart
+++ lib/features/home/presentation/screen/widgets/home_widget.dart
@@ -7,41 +7,149 @@ class HomeWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Material(
-          elevation: 10.0,
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          child: Container(
-            // color: Colors.blue,
-            height: 200,
-            width: 300,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-                colors: [
-                  Color(0xffE6F0EB),
-                  Color(0xff8ACAA8),
-                ],
-              ),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Column(
+        const SizedBox(
+          height: 20,
+        ),
+        Stack(
+          children: [
+            Align(
+              alignment: AlignmentDirectional.center,
+              child: Material(
+                elevation: 10.0,
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                child: Container(
+                  // color: Colors.blue,
+                  height: 140,
+                  width: 310,
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Color(0xffE6F0EB),
+                        Color(0xff8ACAA8),
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Row(
                     children: [
-                      Text("Hey, Get to know your plant identity!"),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Text("Click here"),
-                      )
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.all(24.0),
+                              child: Text(
+                                "Hey, Get to know your plant identity!",
+                                textAlign: TextAlign.left,
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Text("Click here"),
+                            )
+                          ],
+                        ),
+                      ),
+                      const Expanded(
+                        flex: 1,
+                        child: SizedBox(),
+                      ),
                     ],
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
+            Positioned(
+              bottom: -33.0,
+              right: -20.0,
+              child: Image.asset(
+                'assets/vector_images/vector_image_2_home.png',
+                height: 200,
+                width: 200,
+              ),
+              // Container(
+              //   color: Colors.red,
+              //   height: 100,
+              //   width: 100,
+              // ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Stack(
+          children: [
+            Align(
+              alignment: AlignmentDirectional.center,
+              child: Material(
+                elevation: 20.0,
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                child: Container(
+                  // color: Colors.blue,
+                  height: 140,
+                  width: 310,
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Color(0xff8ACAA8),
+                        Color(0xffE6F0EB),
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Row(
+                    children: [
+                      const Expanded(
+                        flex: 1,
+                        child: SizedBox(),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.all(24.0),
+                              child: Text(
+                                "Hey, Get to know your plant identity!",
+                                textAlign: TextAlign.left,
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Text("Click here"),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: -33.0,
+              left: -20.0,
+              child: Image.asset(
+                'assets/vector_images/vector_image_2_home.png',
+                height: 200,
+                width: 200,
+              ),
+              // Container(
+              //   color: Colors.red,
+              //   height: 100,
+              //   width: 100,
+              // ),
+            ),
+          ],
         ),
       ],
     );
