4216dea538a80882079143f6a87f23bf7c40a600
Nafiz <shahnafizmanjoor@gmail.com>
2025-02-19 00:40:44 +0600

    chore: pubspec lock file removed

--- lib/core/data/http_client_registry.dart
+++ lib/core/data/http_client_registry.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
-import 'package:urban_greenie/core/data/base_config.dart';
+import 'package:urban_greenie/core/data/http_confige/base_config.dart';
 import 'package:urban_greenie/core/injector/auto_injector.dart';
 import 'package:urban_greenie/core/resource/style/string.dart';
 
